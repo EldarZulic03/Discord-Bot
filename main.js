eb2d89a6095267e7f37bf53cea6a398d22d79366
Eldar Zulic <69102151+EldarZulic03@users.noreply.github.com>
2020-09-14 18:50:36 -0400

    Update main.js
    Added token login and prefix

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const prefix = '.';
+//prefix required before each message
+const fs = require('fs');
+
+client.once('ready', () => {
+    console.log('Re-Mined is online!')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login('NzU1MTk2ODU1MTgwMzI5MDYy.X1_x4Q.yINcOaKUz9cSgZMyiXPpYslHoPU');
+//Login token, has to be last line in the file
